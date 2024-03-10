7f0852aec3778694b576e7d70fb13c89ac2f6c78
Gustavo de Araujo Oliveira <gustavo.dearaujo26@gmail.com>
2024-03-09 22:01:14 -0300

    [Site] Add title to home page

--- Site/src/pages/home.tsx
+++ Site/src/pages/home.tsx
@@ -3,7 +3,7 @@ import GetEventbusQueueResponse from "../interfaces/responses/eventbus-queue/get
 import { EventBusQueueService } from "../services/eventbus-queue-service";
 import GetEventBusQueueListRequest from "../interfaces/requests/eventbus-queue/get-eventbus-queue-list-request";
 import EventBusQueueCard from "./event-bus-queues/eventbus-queue-card";
-import { Grid } from "@mui/material";
+import { Card, CardHeader, Grid } from "@mui/material";
 import AppBreadcrumbItem from "../interfaces/app-breadcrumb-item";
 import { HomeOutlined } from "@mui/icons-material";
 import AppBreadcrumb from "../components/app-breadcrumb";
@@ -57,6 +57,11 @@ const Home = () => {
         <>
             <AppBreadcrumb breadcrumbItems={breadcrumbItems} />
             <Grid container justifyContent='center' spacing={2}>
+                <Grid item md={12}>
+                    <Card>
+                        <CardHeader title="Some registered queues" sx={{textAlign: 'center'}} />
+                    </Card>
+                </Grid>
                 {queues && queues.map(queue => <Grid key={queue.id} item xs={6} md={4}>
                     <EventBusQueueCard queue={queue} showDescription={false} showSummarization={false} showNavigation={true} />
                 </Grid>)}
